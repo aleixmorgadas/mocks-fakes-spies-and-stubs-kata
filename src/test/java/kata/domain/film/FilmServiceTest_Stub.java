3c03becaaa1c4bb008e5f264ea3b6de0b2228727
Aleix <aleixmp@users.noreply.github.com>
2019-10-23 00:27:16 +0200

    Created FilmServiceTest_Stub

---
+++ src/test/java/kata/domain/film/FilmServiceTest_Stub.java
@@ -0,0 +1,56 @@
+package kata.domain.film;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static kata.domain.film.FilmDummy.randomFilm;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class FilmServiceTest_Stub {
+    private static Film randomFilm = randomFilm().build();
+
+    @Test
+    void shouldReturnAFilmAtRepository() {
+        final FilmService filmService = new FilmService(new FilmRepositoryReturningValue_Stub());
+
+        final String title = Faker.instance().funnyName().name();
+        assertEquals(randomFilm, filmService.findById(title).get());
+    }
+
+    @Test
+    void shouldReturnAnEmptyOptionalWhenItIsNotPresent() {
+        final FilmService filmService = new FilmService(new FilmRepositoryReturningEmpty_Stub());
+
+        final String title = Faker.instance().funnyName().name();
+        assertFalse(filmService.findById(title).isPresent());
+    }
+
+    static class FilmRepositoryReturningValue_Stub implements FilmRepository {
+
+        @Override
+        public void save(String s, Film film) {
+
+        }
+
+        @Override
+        public Optional<Film> findById(String s) {
+            return Optional.of(randomFilm);
+        }
+    }
+
+    static class FilmRepositoryReturningEmpty_Stub implements FilmRepository {
+
+        @Override
+        public void save(String s, Film film) {
+
+        }
+
+        @Override
+        public Optional<Film> findById(String s) {
+            return Optional.empty();
+        }
+    }
+}
